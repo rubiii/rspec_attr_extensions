63eebb4678d4e6a3b5012379c648267ac808484e
rubiii <me@rubiii.com>
2010-01-24 15:51:34 +0100

    speedup with method_defined? (thanks abeiros)

--- lib/rspec_attr_extensions.rb
+++ lib/rspec_attr_extensions.rb
@@ -3,7 +3,7 @@ def it_should_have_an_attr_reader_for(*one_or_more_fields)
   
   one_or_more_fields.each do |field|
     it "should have an attr_reader for #{field}" do
-      model.instance_methods.should include(field.to_s)
+      model.method_defined? field
     end
   end
 end
@@ -13,7 +13,7 @@ def it_should_have_an_attr_writer_for(*one_or_more_fields)
   
   one_or_more_fields.each do |field|
     it "should have an attr_writer for #{field}" do
-      model.instance_methods.should include("#{field}=")
+      model.method_defined? "#{field}="
     end
   end
 end
